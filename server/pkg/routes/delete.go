b27f6152b0cce9122a9d1f7ea2398d4839619189
david cañas <dcanasp@unal.edu.co>
2024-03-31 21:42:54 -0500

    batch Delete

--- server/pkg/routes/delete.go
+++ server/pkg/routes/delete.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"encoding/json"
 	logs "global/logging"
 	"global/pkg/awsConfig"
 	"net/http"
 	"stream/pkg/routes/res"
+	"sync"
 
 	"github.com/gorilla/mux"
 )
@@ -40,6 +42,60 @@ func Delete(r *mux.Router) {
 			res.Response_Error(w, "could not delete item on s3")
 			return
 		}
+		res.Response_Success(w)
+
+	}).Methods("DELETE")
+
+	type batchDeleteBody struct {
+		UserId   string   `json:"userIds"`
+		VideoIds []string `json:"videoIds"`
+	}
+	r.HandleFunc("/Batch/", func(w http.ResponseWriter, r *http.Request) {
+		//como delete de dynamo y s3
+
+		var body batchDeleteBody
+		errBody := json.NewDecoder(r.Body).Decode(&body)
+		if errBody != nil {
+			res.Response_Error(w, "Invalid request body")
+			return
+		}
+		dynamoClient := *awsConfig.DynamoClient
+		s3Client := *awsConfig.S3Client
+
+		deleteSuccess := make(chan bool)
+		var deleteWg sync.WaitGroup
+
+		// Deleting items from DynamoDB and S3 concurrently
+		for _, videoId := range body.VideoIds {
+			deleteWg.Add(1)
+			go func(userId, videoId string) {
+				defer deleteWg.Done()
+
+				// Delete from DynamoDB
+				if err := dynamoClient.DeleteEntry(videoId); err != nil {
+					logs.E.Printf("Dynamo entry could not be deleted: %v", err)
+					return
+				}
+
+				// Delete from S3
+				if err := s3Client.DeleteItem(body.UserId, videoId); err != nil {
+					logs.E.Printf("Item could not be deleted from S3: %v", err)
+					return
+				}
+
+				deleteSuccess <- true
+			}(body.UserId, videoId)
+		}
+		// Wait for all deletion operations to finish
+		go func() {
+			deleteWg.Wait()
+			close(deleteSuccess)
+		}()
+
+		for range deleteSuccess {
+			//si quiero verificar que llegen
+		}
+
 		res.Response_Success(w)
 
 	}).Methods("DELETE")
